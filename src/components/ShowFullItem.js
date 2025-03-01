8c34f53668cd96e5fc64e6357f5e8999e4e57da3
valeriya <valeriyastoyan26@gmail.com>
2025-03-01 11:55:34 +0300

    add modal window for items

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -4,11 +4,13 @@ class ShowFullItem extends Component {
     render() {
         return (
             <div className='full-item'>
-                <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} alt="img"/>
-                <h2>{this.props.item.title}</h2>
-                <p>{this.props.item.desc}</p>
-                <b>{this.props.item.price}$</b>
-                <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+                <div>
+                    <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} alt="img"/>
+                    <h2>{this.props.item.title}</h2>
+                    <p>{this.props.item.desc}</p>
+                    <b>{this.props.item.price}$</b>
+                    <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+                </div>
             </div>
           )
     }
